d8a3704f3c52cef39a9c75da2352696c9d9d5e0d
sci10n <martin.estgren@gmail.com>
2024-07-29 19:11:14 +0200

    fix colors for the stages and hide intermediate levels

--- jukuloop/src/utils/srs.ts
+++ jukuloop/src/utils/srs.ts
@@ -3,40 +3,40 @@ import {Stage} from "../types/Srs";
 export const stage_color = (stage: Stage) => {
         switch (stage) {
             case Stage.Apprentice1:
-                return "#D81B60";
+                return "#8BC34A";
             case Stage.Apprentice2:
-                return "#D81B60";
+                return "#4CAF50";
             case Stage.Apprentice3:
-                return "#D81B60";
+                return "#388E3C";
             case Stage.Apprentice4:
-                return "#D81B60";
+                return "#1B5E20";
             case Stage.Guru1:
-                return "#1E88E5";
+                return "#64B5F6";
             case Stage.Guru2:
-                return "#1E88E5";
+                return "#2196F3";
             case Stage.Master:
-                return "#004D40";
+                return "#1A237E";
             case Stage.Enlightened:
-                return "#EBDACC";
+                return "#7B1FA2";
             case Stage.Burned:
-                return "#360C2D";
+                return "#D32F2f";
         }
     }
 
 export const stage_name = (stage: Stage) => {
         switch (stage) {
             case Stage.Apprentice1:
-                return "Apprentice 1";
+                return "Apprentice";
             case Stage.Apprentice2:
-                return "Apprentice 2";
+                return "Apprentice";
             case Stage.Apprentice3:
-                return "Apprentice 3";
+                return "Apprentice";
             case Stage.Apprentice4:
-                return "Apprentice 4";
+                return "Apprentice";
             case Stage.Guru1:
-                return "Guru 1";
+                return "Guru";
             case Stage.Guru2:
-                return "Guru 2";
+                return "Guru";
             case Stage.Master:
                 return "Master";
             case Stage.Enlightened:
