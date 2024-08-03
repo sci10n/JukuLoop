d2af6bf9762416b2a1accaaf076162c15c0c34fe
sci10n <martin.estgren@gmail.com>
2024-08-04 00:27:52 +0200

    add new build

---
+++ docs/_app/immutable/chunks/paths.BpLXwe-_.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_3utfjr)==null?void 0:s.base)??"/JukuLoop";var t;const e=((t=globalThis.__sveltekit_3utfjr)==null?void 0:t.assets)??a;export{e as a,a as b};
---
+++ docs/_app/immutable/entry/start.DDQXANQb.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DxLz3P9L.js";export{t as start};
